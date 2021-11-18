d866a3d5bcdaeb709786c6103bd46b5d107b898d
Yakov Tenilin <yakov@tenilin.ru>
2021-11-18 18:58:37 +0300

    Add test for message send

--- src/test/java/Server/ConnectionTest.java
+++ src/test/java/Server/ConnectionTest.java
@@ -1,29 +1,48 @@
 package Server;
 
 import org.junit.Test;
-
-import javax.net.SocketFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ConnectionTest {
-
-
-
     @Test
-    public void sendMsg() throws IOException {
-
+    public void sendMsg() {
+        Server server = mock(Server.class);
+        ServerSocket serverSocket = mock(ServerSocket.class);
         Socket socket = mock(Socket.class);
 
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        try {
+            when(serverSocket.accept()).thenReturn(socket);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
 
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            when(socket.getOutputStream()).thenReturn(out);
 
+            InputStream in = new ByteArrayInputStream("Hello".getBytes(StandardCharsets.UTF_8));
+            when(socket.getInputStream()).thenReturn(in);
 
+            when(socket.isClosed()).thenReturn(false);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        Connection connection = new Connection(socket, server);
+        connection.start();
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String msg = reader.readLine();
+            assertEquals("Hello", msg);
+        }catch (IOException e) {
+            fail(e.getMessage());
+        }
     }
-
 }
